50eea9fb95c569f1a7e5d0e10136609e984fb75a
shahinusbd <122728277+shahinusbd@users.noreply.github.com>
2024-09-24 11:01:36 +0600

    Initial project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/primereact.min.css";
+import "primereact/resources/themes/lara-light-blue/theme.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PrimeReactProvider>
+      <Component {...pageProps} />
+    </PrimeReactProvider>
+  );
 }
